e485ad7279d996765b878f3d0c09010a281d89a9
noyeong <noyeong1994@gmail.com>
2024-05-07 17:28:46 +0900

    .

--- 팀프로젝트/03.구현소스/data/common_data.js
+++ 팀프로젝트/03.구현소스/data/common_data.js
@@ -19,7 +19,7 @@ const socialLinks = [
 const searchBox = ['Search','Login'];
 
 const navMenu = [
-    { menu:'bad batch', link: "#" },
+    { menu:'bad batch', link: "sub_badbatch.html" },
     { menu:'the-mandalorian', link: "sub_themandalorian.html" },
     { menu:'Ahsoka', link: "#" },
   ];
