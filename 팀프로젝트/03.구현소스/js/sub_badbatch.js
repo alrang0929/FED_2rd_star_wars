9fd004546e66d437373b48875275c2ec70311e2b
ParkSuMin0611 <sellbell07@gmail.com>
2024-05-09 17:09:52 +0900

    asd

--- 팀프로젝트/03.구현소스/js/sub_badbatch.js
+++ 팀프로젝트/03.구현소스/js/sub_badbatch.js
@@ -30,7 +30,7 @@ listMap.posItems();
   //btn에 ele, idx 요소를 가지고 forEach 돈다. idx < 버튼 순서 때문
   btn.forEach((ele, idx) => {
     //ele에 클릭 이벤트가 발생하면 이벤트 할당
-    ele.onclick = (e) => {
+    ele.onmouseenter = (e) => {
       //preventDefault: 윈도우 기본값 설정 끔, 기본값 변경해야할 경우 사용
       e.preventDefault();
       inner.style.top = -100 * idx + "%";
