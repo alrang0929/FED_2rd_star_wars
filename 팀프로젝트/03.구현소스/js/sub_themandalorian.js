16faf8955245e1a84803345aa897c576f097bfb7
LEE WOOJIN <24864@naver.com>
2024-05-09 17:43:38 +0900

    pj

--- 팀프로젝트/03.구현소스/js/sub_themandalorian.js
+++ 팀프로젝트/03.구현소스/js/sub_themandalorian.js
@@ -375,7 +375,7 @@ function galleryGoSlide() {
 
     setTimeout(() => {
       galSlide.appendChild(galSlide.querySelectorAll("li")[0]);
-      galSlide.style.left = "0";
+      galSlide.style.left = "0%";
       galSlide.style.transition = "none";
     }, 500);
 
@@ -396,7 +396,7 @@ function galleryGoSlide() {
     galPgNum == 1 ? (galPgNum = galPgTotal) : galPgNum--;
 
     setTimeout(() => {
-      galSlide.style.left = "0";
+      galSlide.style.left = "0%";
       galSlide.style.transition = ".6s ease-in-out";
     }, 0);
 
@@ -421,7 +421,7 @@ function galleryGoSlide() {
     //btn에 ele, idx 요소를 가지고 forEach 돈다. idx < 버튼 순서 때문
     btn.forEach((ele, idx) => {
       //ele에 클릭 이벤트가 발생하면 이벤트 할당
-      ele.onclick = (e) => {
+      ele.onmouseenter = (e) => {
         //preventDefault: 윈도우 기본값 설정 끔, 기본값 변경해야할 경우 사용
         e.preventDefault();
         inner.style.top = -100 * idx + "%";
