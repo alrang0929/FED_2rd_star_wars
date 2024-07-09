f9837e95e80dadc4bac07d7037442f6c6a0d6f33
ParkSuMin0611 <sellbell07@gmail.com>
2024-07-09 12:25:34 +0900

    sad

---
+++ 000_React_PJ/star-app/src/components/module/Sub_bt_banner.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//svg
+import { ReactComponent as arrow } from "/images/arrow.svg";
+import { ReactComponent as arrow2 } from "/images/arrow2.svg";
+import { ReactComponent as crose } from "/images/crose.svg";
+//////import area//////////////////////////
+
+function Sub_bt_banner() {
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default Sub_bt_banner;
---
+++ 000_React_PJ/star-app/src/components/page/Badbatch.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Badbatch(props) {
+    return (
+        <>
+            {/* 1. 상단 타이틀 영역 */}
+            {/* 2. 서브 컨텐츠: info area */}
+            {/* 3. 서브 컨텐츠: episode-area */}
+            {/* 4. 서브 컨텐츠: pos-area */}
+            {/* 5. 서브 공통모듈: 하단배너 / bt-banner-area */}
+
+        </>
+    );
+}
+
+export default Badbatch;
