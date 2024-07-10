be1ca05c063b801afeecc61fbe3233dee17bffd4
LEE WOOJIN <24864@naver.com>
2024-07-10 14:39:44 +0900

    Update Themandalorian.jsx

--- 000_React_PJ/star-app/src/components/page/Themandalorian.jsx
+++ 000_React_PJ/star-app/src/components/page/Themandalorian.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 function Themandalorian(props) {
-    return (
-        <div>
-            ddd
-        </div>
-    );
+  return (
+    <main className="main-area">
+      {/* <!-- 인포영역 --> */}
+      {/* <!-- 에피소드 / 뉴스  영역 --> */}
+      {/* <!-- 비디오 영역 --> */}
+      {/* <!-- 캐릭터 영역 --> */}
+      {/* <!-- 갤러리 영역 --> */}
+    </main>
+  );
 }
 
 export default Themandalorian;
