44e36572d86f25087fac5ab230db337ae44a371f
LEE WOOJIN <24864@naver.com>
2024-07-10 18:15:23 +0900

    pj

--- 000_React_PJ/star-app/src/components/module/MdCharacter.jsx
+++ 000_React_PJ/star-app/src/components/module/MdCharacter.jsx
@@ -1,24 +1,90 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { characterData } from "../../data/sub_themandalorian_data";
+
 
 import "../../css/md_character.scss";
 
+
+import domFn from "../../js/my_function";
+
 function MdCharacter(props) {
+
+  useLayoutEffect(()=>{
+    const crtImgBox = domFn.qs(".character-img-box");
+    
+let target = domFn.qsEl(crtImgBox, ".list01");
+let target2 = domFn.qsEl(crtImgBox, ".list02");
+// 기준값 업데이트 함수
+const updateCriteria = () => domFn.qsaEl(target, "li")[0].offsetWidth;
+const updateCriteria2 = () => domFn.qsaEl(target2, "li")[0].offsetWidth;
+// 기준값(대상 li의 가로크기값)
+let criteria = updateCriteria();
+let criteria2 = updateCriteria2();
+
+domFn.addEvt(window, "resize", () => {
+  criteria = updateCriteria();
+  criteria2 = updateCriteria2();
+});
+
+let currVal = 0;
+let currVal2 = 0;
+
+function moveGallery() {
+  target.style.translate = --currVal + "px";
+
+  if (currVal <= Math.floor(-criteria)) {
+    target.appendChild(domFn.qsaEl(target, "li")[0]);
+    target.style.translate = "0px";
+    currVal = 0;
+  } /// if///
+
+  if (!stopSts) setTimeout(moveGallery, 6);
+} ////// moveGallery 함수 //////
+
+function moveGallery2() {
+  target2.style.translate = --currVal2 + "px";
+
+  if (currVal2 <= Math.floor(-criteria2)) {
+    target2.appendChild(domFn.qsaEl(target2, "li")[0]);
+    target2.style.translate = "0px";
+    currVal2 = 0;
+  } /// if///
+
+  if (!stopSts) setTimeout(moveGallery2, 10);
+} /// moveGallery2 함수
+
+let stopSts = false;
+
+domFn.addEvt(crtImgBox, "mouseenter", () => {
+  stopSts = true;
+});
+
+domFn.addEvt(crtImgBox, "mouseleave", () => {
+  stopSts = false;
+  moveGallery();
+  moveGallery2();
+});
+
+setTimeout(moveGallery);
+setTimeout(moveGallery2);
+    
+
+  },[])
   return (
     <div id="character-area">
-      <section class="character-area section-top">
-        <div class="inboxman character-wrap">
+      <section className="character-area section-top">
+        <div className="inboxman character-wrap">
           <h2>
             <span>Databank: The Mandalorian</span>
           </h2>
-          <div class="character-img-box">
-            <div class="img-list">
-              <ul class="list01">
+          <div className="character-img-box">
+            <div className="img-list">
+              <ul className="list01">
                 {characterData.map(
                   (v, i) =>
                     i < 15 && (
                       <li key={i}>
-                        <div class="list-box">
+                        <div className="list-box">
                           <img
                             src={"/images/sub_themandalorian_images/" + v.image}
                             alt=""
@@ -33,13 +99,13 @@ function MdCharacter(props) {
               </ul>
             </div>
 
-            <div class="img-list">
-              <ul class="list02">
+            <div className="img-list">
+              <ul className="list02">
                 {characterData.map(
                   (v, i) =>
                     i < 30 && i>=14 && (
                       <li key={i}>
-                        <div class="list-box">
+                        <div className="list-box">
                           <img
                             src={"/images/sub_themandalorian_images/" + v.image}
                             alt=""
--- 000_React_PJ/star-app/src/components/module/MdEpisode.jsx
+++ 000_React_PJ/star-app/src/components/module/MdEpisode.jsx
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { episodeData } from "../../data/sub_themandalorian_data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import domFn from "../../js/my_function";
 
 import "../../css/md_episode.scss";
 
 function MdEpisode(props) {
+  useLayoutEffect(()=>{
+    const epbtn = domFn.qsa(".epbtn");
+    const vdbtn = domFn.qsa(".vdbtn");
+    const epSlide = domFn.qsa(".ep-img-wrap ul li");
+    const epWrap = domFn.qs(".ep-wrap");
+   
+let epSnum = 0;
+let vdSnum = 0;
+let prot = false;
+
+for (let x of epbtn) {
+  x.onclick = GoEpSlide;
+}
+
+
+function GoEpSlide() {
+  // 광클 금지
+  if (prot) return;
+  prot = true;
+  setTimeout(() => {
+    prot = false;
+  }, 500);
+
+  let isEpRbtn = this.classList.contains("epbtn2");
+  let cntNum;
+  let epSlCnt = epSlide.length;
+
+  isEpRbtn ? epSnum++ : epSnum--;
+
+  epSlide.forEach((v) => {
+    if (epWrap.offsetWidth > 1024) {
+      v.style.left = -33.3333 * epSnum + "%";
+      cntNum = 3;
+    } else if (epWrap.offsetWidth > 860) {
+      v.style.left = -50 * epSnum + "%";
+      cntNum = 2;
+    } else {
+      v.style.left = -100 * epSnum + "%";
+      cntNum = 1;
+    }
+  });
+
+  if (epSnum < 0) {
+    epSnum = 0;
+  } else if (epSnum > epSlCnt - cntNum) {
+    epSnum = epSlCnt - cntNum;
+  }
+
+  if (epSnum == 0 || epSnum == epSlCnt - cntNum) {
+    this.style.display = "none";
+  } else {
+    for (let x of epbtn) {
+      x.style.display = "block";
+    }
+  }
+}
+  })
   return (
     <div id="episode-area">
       <section className="episode-area cont-box section-top">
