7fb941ada57033e60301915106ff1bd8b8db1de6
ParkSuMin0611 <sellbell07@gmail.com>
2024-07-12 09:32:34 +0900

    adsad

--- 000_React_PJ/star-app/src/components/page/Badbatch.jsx
+++ 000_React_PJ/star-app/src/components/page/Badbatch.jsx
@@ -1,5 +1,5 @@
 ///스타워즈PJ - 서브페이지 Badbatch
-import React from 'react';
+import React, { Fragment } from 'react';
 //css
 import "../../css/sub_badbatch.scss";
 
@@ -14,7 +14,7 @@ import BbPosterArea from '../module/BbPosterArea';
 function Badbatch() {
     ///////////코드 리턴구역 /////////////////////////////////////
     return (
-        <>
+        <Fragment className="badbatch">
             {/* 1. 상단 타이틀 영역 */}
             <BbTopBanner/>
             {/* 2. 서브 컨텐츠: info area */}
@@ -27,7 +27,7 @@ function Badbatch() {
             <BbPosterArea/>
             {/* 6. 서브 공통모듈: 하단배너 / bt-banner-area */}
             {/* <SubBtBanner/> */}
-        </>
+        </Fragment>
     );
 }
 
