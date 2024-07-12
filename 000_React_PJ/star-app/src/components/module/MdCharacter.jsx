50cd46f956931f330c0c5fa842476ce5a37e9734
LEE WOOJIN <24864@naver.com>
2024-07-12 12:34:39 +0900

    up

--- 000_React_PJ/star-app/src/components/module/MdCharacter.jsx
+++ 000_React_PJ/star-app/src/components/module/MdCharacter.jsx
@@ -1,75 +1,70 @@
 import React, { useEffect, useLayoutEffect } from "react";
 import { characterData } from "../../data/sub_themandalorian_data";
 
-
 import "../../css/md_character.scss";
-
 
 import domFn from "../../js/my_function";
 
 function MdCharacter(props) {
-
-  useLayoutEffect(()=>{
+  useLayoutEffect(() => {
     const crtImgBox = domFn.qs(".character-img-box");
-    
-let target = domFn.qsEl(crtImgBox, ".list01");
-let target2 = domFn.qsEl(crtImgBox, ".list02");
-// 기준값 업데이트 함수
-const updateCriteria = () => domFn.qsaEl(target, "li")[0].offsetWidth;
-const updateCriteria2 = () => domFn.qsaEl(target2, "li")[0].offsetWidth;
-// 기준값(대상 li의 가로크기값)
-let criteria = updateCriteria();
-let criteria2 = updateCriteria2();
 
-domFn.addEvt(window, "resize", () => {
-  criteria = updateCriteria();
-  criteria2 = updateCriteria2();
-});
+    let target = domFn.qsEl(crtImgBox, ".list01");
+    let target2 = domFn.qsEl(crtImgBox, ".list02");
+    // 기준값 업데이트 함수
+    const updateCriteria = () => domFn.qsaEl(target, "li")[0].offsetWidth;
+    const updateCriteria2 = () => domFn.qsaEl(target2, "li")[0].offsetWidth;
+    // 기준값(대상 li의 가로크기값)
+    let criteria = updateCriteria();
+    let criteria2 = updateCriteria2();
 
-let currVal = 0;
-let currVal2 = 0;
+    domFn.addEvt(window, "resize", () => {
+      criteria = updateCriteria();
+      criteria2 = updateCriteria2();
+    });
 
-function moveGallery() {
-  target.style.translate = --currVal + "px";
+    let currVal = 0;
+    let currVal2 = 0;
 
-  if (currVal <= Math.floor(-criteria)) {
-    target.appendChild(domFn.qsaEl(target, "li")[0]);
-    target.style.translate = "0px";
-    currVal = 0;
-  } /// if///
+    function moveGallery() {
+      target.style.translate = --currVal + "px";
 
-  if (!stopSts) setTimeout(moveGallery, 6);
-} ////// moveGallery 함수 //////
+      if (currVal <= Math.floor(-criteria)) {
+        target.appendChild(domFn.qsaEl(target, "li")[0]);
+        target.style.translate = "0px";
+        currVal = 0;
+      } /// if///
 
-function moveGallery2() {
-  target2.style.translate = --currVal2 + "px";
+      if (!stopSts) setTimeout(moveGallery, 6);
+    } ////// moveGallery 함수 //////
 
-  if (currVal2 <= Math.floor(-criteria2)) {
-    target2.appendChild(domFn.qsaEl(target2, "li")[0]);
-    target2.style.translate = "0px";
-    currVal2 = 0;
-  } /// if///
+    function moveGallery2() {
+      target2.style.translate = --currVal2 + "px";
 
-  if (!stopSts) setTimeout(moveGallery2, 10);
-} /// moveGallery2 함수
+      if (currVal2 <= Math.floor(-criteria2)) {
+        target2.appendChild(domFn.qsaEl(target2, "li")[0]);
+        target2.style.translate = "0px";
+        currVal2 = 0;
+      } /// if///
 
-let stopSts = false;
+      if (!stopSts) setTimeout(moveGallery2, 10);
+    } /// moveGallery2 함수
 
-domFn.addEvt(crtImgBox, "mouseenter", () => {
-  stopSts = true;
-});
+    let stopSts = false;
 
-domFn.addEvt(crtImgBox, "mouseleave", () => {
-  stopSts = false;
-  moveGallery();
-  moveGallery2();
-});
+    domFn.addEvt(crtImgBox, "mouseenter", () => {
+      stopSts = true;
+    });
 
-setTimeout(moveGallery);
-setTimeout(moveGallery2);
-    
+    domFn.addEvt(crtImgBox, "mouseleave", () => {
+      stopSts = false;
+      moveGallery();
+      moveGallery2();
+    });
 
-  },[])
+    setTimeout(moveGallery);
+    setTimeout(moveGallery2);
+  }, []);
   return (
     <div id="character-area">
       <section className="character-area section-top">
@@ -103,7 +98,8 @@ setTimeout(moveGallery2);
               <ul className="list02">
                 {characterData.map(
                   (v, i) =>
-                    i < 30 && i>=14 && (
+                    i < 30 &&
+                    i >= 14 && (
                       <li key={i}>
                         <div className="list-box">
                           <img
