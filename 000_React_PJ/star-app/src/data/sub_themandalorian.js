a58500caf7633637bdb51bd6ba769dbd1dd3ebb2
LEE WOOJIN <24864@naver.com>
2024-07-16 09:39:10 +0900

    Update sub_themandalorian.js

--- 000_React_PJ/star-app/src/data/sub_themandalorian.js
+++ 000_React_PJ/star-app/src/data/sub_themandalorian.js
@@ -3,23 +3,21 @@ import domFn from "../js/my_function";
 import { videoData } from "./sub_themandalorian_data";
 
 function mandalvideo() {
-  
-    
-    const vdbtn = domFn.qsa(".vdbtn");
-    
-    const videoWrap = domFn.qs(".video-wrap");
-    const videoli = domFn.qs(".video-list");
-    const videoBox = domFn.qs(".video-cont-box");
-    const videoTab = domFn.qs(".video-tab");
+  const vdbtn = domFn.qsa(".vdbtn");
 
-    const vdData = videoData;
+  const videoWrap = domFn.qs(".video-wrap");
+  const videoli = domFn.qs(".video-list");
+  const videoBox = domFn.qs(".video-cont-box");
+  const videoTab = domFn.qs(".video-tab");
 
-    // console.log(videoWrap.offsetWidth);
-    // videoWrap.offsetWidth;
+  const vdData = videoData;
 
-    let vlicode = `<ul class="fx-box">`;
-    for (let i = 0; i <= 10; i++) {
-      vlicode += `
+  // console.log(videoWrap.offsetWidth);
+  // videoWrap.offsetWidth;
+
+  let vlicode = `<ul class="fx-box">`;
+  for (let i = 0; i <= 10; i++) {
+    vlicode += `
     <li>
       <div class="mdvideo-box">
         <div class="video-img">
@@ -35,84 +33,94 @@ function mandalvideo() {
       </div>
     </li>
   `;
-    }
-    vlicode += `</ul>`;
+  }
+  vlicode += `</ul>`;
 
-    videoli.innerHTML = vlicode;
+  videoli.innerHTML = vlicode;
 
-    const vdSlide = domFn.qsaEl(videoWrap, ".video-img-wrap ul li");
-    const video = domFn.qsaEl(videoli, ".mdvideo-box");
+  const vdSlide = domFn.qsaEl(videoWrap, ".video-img-wrap ul li");
+  const video = domFn.qsaEl(videoli, ".mdvideo-box");
 
-    let vdcode = ``;
-    video.forEach((ele, i) => {
-      ele.onclick = () => {
-        vdcode += `
+  let vdcode = ``;
+  video.forEach((ele, i) => {
+    ele.onclick = () => {
+      vdcode += `
     <div class="video-tab-btn fa-solid fa-xmark fa-3x"></div>
     <iframe src="https://www.youtube.com/embed/${vdData[i].video}?autoplay=1" allow="autoplay""></iframe>
   `;
-        videoTab.innerHTML = vdcode;
-        videoBox.classList.add("on");
-        // body.style.overflow = "hidden";
+      videoTab.innerHTML = vdcode;
+      videoBox.classList.add("on");
+      // body.style.overflow = "hidden";
 
-        domFn.qsEl(videoTab, "div").onclick = () => {
-          console.log("ddd");
-          videoBox.classList.remove("on");
-          vdcode = ``;
-          // body.style.overflow = "auto";
-        };
+      domFn.qsEl(videoTab, "div").onclick = () => {
+        console.log("ddd");
+        videoBox.classList.remove("on");
+        vdcode = ``;
+        // body.style.overflow = "auto";
       };
-    });
-  
+    };
+  });
 
-    let vdSnum = 0;
-    let prot = false;
-    for (let x of vdbtn) {
-      x.onclick = GoVdSlide;
-    }
-    function GoVdSlide() {
-      // 광클 금지
-      if (prot) return;
-      prot = true;
-      setTimeout(() => {
-        prot = false;
-      }, 500);
+  let vdSnum = 0;
+  let prot = false;
+  for (let x of vdbtn) {
+    x.onclick = GoVdSlide;
+  }
+  function GoVdSlide() {
+    // 광클 금지
+    if (prot) return;
+    prot = true;
+    setTimeout(() => {
+      prot = false;
+    }, 500);
 
-      let isVdRbtn = this.classList.contains("vdbtn2");
+    let isVdRbtn = this.classList.contains("vdbtn2");
 
-      let cntNum;
-      let vdSlCnt = 11;
-      console.log(vdSlCnt);
+    let cntNum;
+    let vdSlCnt = 11;
+    console.log(vdSlCnt);
 
-      isVdRbtn ? vdSnum++ : vdSnum--;
+    isVdRbtn ? vdSnum++ : vdSnum--;
 
-      vdSlide.forEach((v) => {
-        if (videoWrap.offsetWidth > 1024) {
-          v.style.left = -33.3333 * vdSnum + "%";
-          cntNum = 3;
-        } else if (videoWrap.offsetWidth > 860) {
-          v.style.left = -50 * vdSnum + "%";
-          cntNum = 2;
-        } else {
-          v.style.left = -100 * vdSnum + "%";
-          cntNum = 1;
-        }
-      });
-
-      if (vdSnum < 0) {
-        vdSnum = 0;
-      } else if (vdSnum > vdSlCnt - cntNum) {
-        vdSnum = vdSlCnt - cntNum;
-      }
-
-      if (vdSnum == 0 || vdSnum == vdSlCnt - cntNum) {
-        this.style.display = "none";
+    vdSlide.forEach((v) => {
+      if (videoWrap.offsetWidth > 1024) {
+        v.style.left = -33.3333 * vdSnum + "%";
+        cntNum = 3;
+      } else if (videoWrap.offsetWidth > 860) {
+        v.style.left = -50 * vdSnum + "%";
+        cntNum = 2;
       } else {
-        for (let x of vdbtn) {
-          x.style.display = "block";
-        }
+        v.style.left = -100 * vdSnum + "%";
+        cntNum = 1;
       }
+    });
+
+    if (vdSnum < 0) {
+      vdSnum = 0;
+    } else if (vdSnum > vdSlCnt - cntNum) {
+      vdSnum = vdSlCnt - cntNum;
     }
 
+    if (vdSnum == 0 || vdSnum == vdSlCnt - cntNum) {
+      this.style.display = "none";
+    } else {
+      for (let x of vdbtn) {
+        x.style.display = "block";
+      }
+    }
+  }
+
+  // 화면 사이즈 줄일때 슬라이드 위치 초기화
+  domFn.addEvt(window, "resize", () => {
+    vdSlide.forEach((v) => {
+      v.style.left = 0;
+    });
+   
+    vdSnum = 0;
+    vdbtn[0].style.display = "none";
+
+    vdbtn[1].style.display = "block";
+  }); ////////////// resize함수 //////////////////
 }
 function mandalgallery(params) {
   let prot = false;
