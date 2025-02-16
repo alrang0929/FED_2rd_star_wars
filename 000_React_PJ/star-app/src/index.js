db3d2e9cd28f9c7d40d1036d65b7ea6702383f30
meow-u <noyeong1994@gmail.com>
2025-02-16 16:25:14 +0900

    .

--- 000_React_PJ/star-app/src/index.js
+++ 000_React_PJ/star-app/src/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect} from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/layout/Layout";
